f78c5d418ac92c7c8fba6438bc31bb6a4a9e47b4
Uanderson Nunes de Lima <uandersonmbc@hotmail.com>
2019-11-21 21:15:53 -0300

    Falta termiar o cadastro e edição

--- src/screens/contents/Product.js
+++ src/screens/contents/Product.js
@@ -1,41 +1,61 @@
 import React, { useEffect, useState } from 'react';
 
+import Moment from 'react-moment';
+
 import Api from './../../services/api';
 
-import { Table, Modal, Button, Row, Icon } from 'antd';
+import { message, Table, Modal, Button, Row, Icon, Form, Select, Popconfirm } from 'antd';
 
-const columns = [
-    {
-        title: 'ID',
-        dataIndex: 'id',
-        key: 'id',
-    },
-    {
-        title: 'Nome',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: 'Preço',
-        dataIndex: 'price',
-        key: 'price',
-    },
-    {
-        title: 'Criado',
-        dataIndex: 'created_at',
-        key: 'created_at',
-    },
-    {
-        title: 'Ações',
-        dataIndex: 'action',
-        key: 'action',
-    },
-];
+const { Option } = Select;
 
-function Product() {
+function Product(props) {
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: 'Nome',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Preço',
+            dataIndex: 'price',
+            key: 'price',
+        },
+        {
+            title: 'Criado',
+            dataIndex: 'created_at',
+            key: 'created_at',
+            render: created_at => (
+                <Moment format="DD/MM/YYYY hh:mm:ss">
+                    {created_at}
+                </Moment>
+            )
+        },
+        {
+            title: 'Ações',
+            dataIndex: 'key',
+            render: key => (
+                <>
+                    <Button type='primary' style={{ marginRight: '5px' }} onClick={() => handleEdit(key)}>Editar</Button>
+                    <Popconfirm onConfirm={() => handleDelete(key)} okText='Sim' cancelText='Não' title="Quer mesmo deletar?">
+                        <Button type='danger' >Deletar</Button>
+                    </Popconfirm>
+                </>
+            )
+        },
+    ];
+
     const [products, setProducts] = useState({
-        data: []
+        data: [],
+        total: 0
     });
+
+    const [ingredients, setIngredients] = useState([]);
+    const [formSub, setFormSub] = useState()
 
     const [loadingData, setLoadingData] = useState(false);
 
@@ -50,31 +70,87 @@ function Product() {
         });
     };
 
-    const handleOk = e => {
-        console.log(e);
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target.teste)
+    };
+
+    const handleEdit = async (id) => {
+        const product = await Api.get('/product/' + id);
+        setFormSub({
+            name: product.data.name,
+            price: product.data.price,
+            category_id: product.data.category_id,
+            ingredients: product.data.ingredients,
+        });
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const response = await Api.delete('/product/' + id);
+            message.success('O produto ' + response.data.name + ' foi deletado com sucesso')
+            setProducts({
+                ...products,
+                data: products.data.filter(product => product.id !== id)
+            });
+        } catch (error) {
+            message.error('Erro na hora de deletar o produto', 2.5)
+        }
+    }
+
+
+    const handleCancel = e => {
         setmodalProduct({
             visible: false,
         });
     };
 
-    const handleCancel = e => {
-        console.log(e);
-        setmodalProduct({
-            visible: false,
-        });
-    };
+    function handleChange(value) {
+        console.log(`selected ${value}`);
+    }
 
     const loadingProducts = async (page = 1) => {
         setLoadingData(true);
-        const response = await Api.get('/product?page=' + page);
-        setProducts(response.data);
+
+        try {
+            const response = await Api.get('/product?page=' + page);
+            const data = response.data.data;
+            var data_fianl = data.map((product) => {
+                product.key = product.id;
+                return product;
+            });
+            setProducts({
+                total: response.data.total,
+                data: data_fianl
+            });
+        } catch (error) {
+            alert('Não foi possível buscar os produtos')
+        }
         setLoadingData(false);
     }
+
+    const loadingIngredients = async (page = 1) => {
+        try {
+            const response = await Api.get('/ingredient');
+
+            let data = response.data.map(ingredient => {
+                return (<Option value={ingredient.name} key={ingredient.id}>{ingredient.name}</Option>)
+            });
+
+            setIngredients(data);
+        } catch (error) {
+            alert('Não foi possível buscar os ingredientes')
+        }
+    }
+
+
     useEffect(() => {
         loadingProducts()
+        loadingIngredients()
     }, []);
 
-    console.log(products.total * 1);
+    console.log(formSub)
+
     return (
         <>
             <Row>
@@ -90,9 +166,21 @@ function Product() {
                 confirmLoading={modalProduct.confirmLoading}
                 footer={[
                     <Button onClick={handleCancel} type='danger' key="a">Cancelar <Icon type="close" /></Button>,
-                    <Button onClick={handleOk} key="b" type="primary">Cadastrar <Icon type="check" /></Button>,
+                    <Button form="formProduct" htmlType="submit" type="primary">Cadastrar <Icon type="check" /></Button>,
                 ]}
             >
+                <Form id="formProduct" onSubmit={handleSubmit}>
+                    <Select
+                        name='teste'
+                        mode="multiple"
+                        style={{ width: '100%' }}
+                        placeholder="Please select"
+                        defaultValue={['china']}
+                        onChange={handleChange}
+                    >
+                        {ingredients}
+                    </Select>
+                </Form>
             </Modal>
             <Table
                 style={{ background: '#fff' }}
@@ -103,7 +191,7 @@ function Product() {
                         onChange: loadingProducts,
                         defaultPageSize: 20,
                         defaultCurrent: products.page,
-                        total: products.total
+                        total: (products.total * 1)
                     }
                 }
                 loading={loadingData}
